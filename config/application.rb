723e7ed1672b3999c25930515874ce44fc3a52b2
Michael Kirk <michael.john.kirk@gmail.com>
2011-02-05 14:36:36 -0600

    added rspec as testing framework

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Dbc
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.generators do |g|
+      g.test_framework = :rspec
+    end
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
