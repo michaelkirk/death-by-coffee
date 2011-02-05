1b743b79dd29b5ab4bb8c73cf800f5155057046e
Michael Kirk <michael.john.kirk@gmail.com>
2011-02-05 14:33:25 -0600

    sterile rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbc::Application.config.secret_token = 'f35acc78dee6afc7718299e464d67385ef5e27fdfb5001a073a88c387b30f54c34cdf741047e8ae06eacf33dd263532c2e73e2e8a85ae4641435c28c3a4f56f2'
